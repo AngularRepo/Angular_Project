41d6fb722b79d9c151a1f1abe3a7f6ce3c00c2ba
AngularRepo <42567660+AngularRepo@users.noreply.github.com>
2018-08-21 13:04:58 +0530

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ThanksComponent } from './thanks/thanks.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'about/:id',
+    component: AboutComponent
+  },
+  {
+    path: 'thanks',
+    component: ThanksComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
